7513c946a716b560cda8483c5072688373e07871
191014146 <19104146@usc.edu.ph>
2024-12-18 00:42:40 +0800

    chore: init fastify app

---
+++ helpdesk/src/index.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.get("/ping", async () => {
+  return "pong\n";
+});
+
+server.listen({ port: 3000 }, (error, address) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log(`Server listening at ${address}`);
+});
