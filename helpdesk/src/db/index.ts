e946c6d9f0b348fcd07b6528bb34427320d3eeae
191014146 <19104146@usc.edu.ph>
2024-12-18 17:20:54 +0800

    feat: dockerize hono app

--- helpdesk/src/db/index.ts
+++ helpdesk/src/db/index.ts
@@ -1,8 +1,6 @@
 import { Database } from "bun:sqlite"
 import { drizzle } from "drizzle-orm/bun-sqlite"
-import { migrate } from "drizzle-orm/bun-sqlite/migrator"
 import * as schema from "./schema"
 
 const sqlite = new Database("sqlite.db")
 export const db = drizzle(sqlite, { schema })
-migrate(db, { migrationsFolder: "./drizzle" })
